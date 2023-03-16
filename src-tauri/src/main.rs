7b9c5dec4533b1b3c8bc3dd4c957ea0d8dfb0499
mojahige <13248811+mojahige@users.noreply.github.com>
2023-03-16 10:05:07 +0900

    Add brick-os-name
    This component formats and displays text from the OS name get from Tauri 👏
    
    https://tauri.app/v1/guides/getting-started/setup/html-css-js/#invoke-commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,18 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use std::env;
+
+#[tauri::command]
+fn os_name() -> String {
+    format!("{}", env::consts::OS)
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![os_name])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
